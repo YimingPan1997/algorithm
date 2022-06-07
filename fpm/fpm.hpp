ba62c672853532cfee3616e1c70cd4180f710b50
Yiming Pan <56082650+YimingPan-Code@users.noreply.github.com>
2022-06-07 09:08:38 -0700

    Update fpm.hpp

--- fpm/fpm.hpp
+++ fpm/fpm.hpp
@@ -66,125 +66,124 @@ namespace UDIMPL {
             return string(result);
 	}
 
-	inline int64_t get_hash(int64_t id) {
-    int64_t hash_val = id % tg::MOD;
-    return hash_val;
-  }
+	inline int64_t get_hash(int64_t id){
+	    int64_t hash_val = id % MOD;
+	    return hash_val;
+	}
 
-  template < typename tup >
-    inline int64_t get_pattern_key(MaxAccum < tup > t) {
-      tup in_data = t;
-      return in_data.pattern_key;
-    }
+	template<typename tup>
+	inline int64_t get_pattern_key(MaxAccum<tup> t){
+		tup in_data = t;
+		return in_data.pattern_key;
+	}
+	
+	template<typename tup>
+	inline int64_t get_suffix_key(MaxAccum<tup> t){
+		tup in_data = t;
+		return in_data.suffix_key;
+	}
+	inline ListAccum<int64_t> initiate_hash_const(int length){
+		ListAccum<int64_t> return_arr;
+		__int128_t promoter;
 
-  template < typename tup >
-    inline int64_t get_suffix_key(MaxAccum < tup > t) {
-      tup in_data = t;
-      return in_data.suffix_key;
-    }
-  inline ListAccum < int64_t > initiate_hash_const(int length) {
-    ListAccum < int64_t > return_arr;
-    __int128_t promoter;
+		return_arr += 1;
 
-    return_arr += 1;
+		for (int i = 1; i < length + 1; i++){
+			promoter = return_arr.get(i - 1);
+			
+			promoter *= BASE;
+			promoter %= MOD;
+			
+			return_arr += static_cast<int64_t>(promoter);
+		}
+		
+		return return_arr;
+	}
+	inline int64_t get_hash_without_first_element(int64_t hash_val, int64_t first, int size, const ListAccum<int64_t>& HASH_CONST){
+	    __int128_t promoter = hash_val;
 
-    for (int i = 1; i < length + 1; i++) {
-      promoter = return_arr.get(i - 1);
+	    __int128_t first_hash = first;
+	    first_hash *= HASH_CONST.get(size - 1);
+	    first_hash %= MOD;
 
-      promoter *= tg::BASE;
-      promoter %= tg::MOD;
+	    promoter -= first_hash;
 
-      return_arr += static_cast < int64_t > (promoter);
-    }
+	    promoter %= MOD;
+	    promoter += MOD;
+	    promoter %= MOD;
 
-    return return_arr;
-  }
-  inline int64_t hash_without_first_element(int64_t hash_val, int64_t first, int size,
-    const ListAccum < int64_t > & HASH_CONST) {
-    __int128_t promoter = hash_val;
+	    promoter *= BASE;
+	    promoter %= MOD;
 
-    __int128_t first_hash = first;
-    first_hash *= HASH_CONST.get(size - 1);
-    first_hash %= tg::MOD;
+	    return static_cast<int64_t>(promoter);
+	}
 
-    promoter -= first_hash;
+	inline int64_t get_hash_without_last_element(int64_t hash_val, int64_t last){
+		__int128_t promoter = hash_val;
+		promoter -= last;
+		promoter %= MOD;
+		promoter += MOD;
+		promoter %= MOD;
 
-    promoter %= tg::MOD;
-    promoter += tg::MOD;
-    promoter %= tg::MOD;
+	    return promoter;
+	}
 
-    promoter *= tg::BASE;
-    promoter %= tg::MOD;
+	inline int64_t get_hash_concat(int64_t hash_val, int64_t appender){
+	    __int128_t promoter = hash_val;
 
-    return static_cast < int64_t > (promoter);
-  }
+	    promoter *= BASE;
+	    promoter %= MOD;
 
-  inline int64_t get_hash_without_last_element(int64_t hash_val, int64_t last) {
-    __int128_t promoter = hash_val;
-    promoter -= last;
-    promoter %= tg::MOD;
-    promoter += tg::MOD;
-    promoter %= tg::MOD;
+	    promoter += appender;
+	    promoter %= MOD; 
 
-    return promoter;
-  }
+	    return static_cast<int64_t>(promoter); 
+	}
 
-  inline int64_t get_hash_concat(int64_t hash_val, int64_t appender) {
-    __int128_t promoter = hash_val;
+	inline bool is_subset(const ListAccum<int64_t>& subset, int64_t ending, ListAccum<int64_t> &superset){
 
-    promoter *= tg::BASE;
-    promoter %= tg::MOD;
+	    int curr_pos = 0;
+	    int i = 0;
 
-    promoter += appender;
-    promoter %= tg::MOD;
+	    for (i = 0; i < superset.size(); ++i){
+		if (superset.get(i) == subset.get(curr_pos)){
+		    ++curr_pos;
+		    if (curr_pos == subset.size()){
+			break;
+		    }
+		}
+	    }
+	    if (curr_pos != subset.size()){
+	    	return false;
+	    }
 
-    return static_cast < int64_t > (promoter);
-  }
+	    for (int j = i + 1; j < superset.size(); ++j){
+		if (superset.get(j) == ending){
+		    return true;
+		}
+	    }
 
-  inline bool is_subset(const ListAccum < int64_t > & subset, int64_t ending, ListAccum < int64_t > & superset) {
+	    return false;
 
-    int curr_pos = 0;
-    int i = 0;
+	}
 
-    for (i = 0; i < superset.size(); ++i) {
-      if (superset.get(i) == subset.get(curr_pos)) {
-        ++curr_pos;
-        if (curr_pos == subset.size()) {
-          break;
-        }
-      }
-    }
-    if (curr_pos != subset.size()) {
-      return false;
-    }
+	inline ListAccum<int64_t> concat(ListAccum<int64_t> original_list, int64_t appending_element){
+	    original_list += appending_element;
 
-    for (int j = i + 1; j < superset.size(); ++j) {
-      if (superset.get(j) == ending) {
-        return true;
-      }
-    }
-
-    return false;
-
-  }
-
-  inline ListAccum < int64_t > concat(ListAccum < int64_t > original_list, int64_t appending_element) {
-    original_list += appending_element;
-
-    return original_list;
-  }
-  inline ListAccum < VERTEX > to_vertex_list(const ListAccum < int64_t > & l) {
-    ListAccum < VERTEX > v_list;
-    for (int i = 0; i < l.size(); ++i) {
-      v_list += VERTEX(l.get(i));
-    }
-    return v_list;
-  }
-  template < typename tup >
-    inline tup get_tup(MaxAccum < tup > val) {
-      tup new_tup = val;
-      return new_tup;
-    }
+	    return original_list;
+	}
+	inline ListAccum<VERTEX> to_vertex_list(const ListAccum<int64_t>& l){
+		ListAccum<VERTEX> v_list;
+		for (int i = 0; i < l.size(); ++i){
+			v_list += VERTEX(l.get(i));
+		}
+		return v_list;
+	}
+	template<typename tup>
+	inline tup get_tup(MaxAccum<tup> val){
+		tup new_tup = val;
+		return new_tup;
+	}
 }
 /****************************************/
 
